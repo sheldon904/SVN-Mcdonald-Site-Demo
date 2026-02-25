5aa6a3f546c4e5c7ee276cd7693bbfb998d7a07b
sheldon904 <128750711+sheldon904@users.noreply.github.com>
2026-02-25 16:37:28 -0500

    pics

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -11,7 +11,7 @@ const TeamPage = () => {
     {
       name: "Bartow McDonald IV",
       role: "Managing Director",
-      image: "https://svnmcdonald.com/wp-content/uploads/2018/11/Bartow-McDonald-Headshot-WEB.jpg",
+      image: "/team/bartow-mcdonald.webp",
       phone: "352.484.8090",
       email: "bartow.mcdonald@svn.com",
       bio: "Bartow McDonald IV serves as managing director for SVN | McDonald & Company in Ocala, FL, where he enjoys working on commercial real estate deals throughout Florida. Prior to joining SVN, McDonald served as the vice president of acquisitions and development for Cope Properties, Inc. in Ocala, Florida where he was responsible for the acquisition, entitlement, and marketing of portfolio and client properties. Previously, McDonald served as the founder and chief executive officer of two start-up companies; Bluewire, a service based electrical solutions company and StoreParts, an e-commerce company that supplied supply chain management technology to the supermarket and food retail industries. Before starting two companies, McDonald spent six years working for a fast-growing international manufacturing firm, where he gained in-depth industrial experience through his leadership positions in manufacturing operations, distribution, logistics and marketing. He has served on the board of directors for RMI (Reciprocal Ministries International), the Ocala Chamber of Commerce, the Central Florida Commercial Association of Realtors, and as chairman of the regional advisory board for RBC Bank. In addition, he has participated as a conference speaker for the Florida Venture Capital Forum, the Food Marketing Institute and has been quoted in the Wall Street Journal, Forbes and the New York Times. McDonald earned his MBA and Bachelor of Science from the University of Florida. Sight fishing and bow hunting are two things that will get him up before sunrise."
@@ -19,7 +19,7 @@ const TeamPage = () => {
     {
       name: "Matthew Garff",
       role: "Associate Advisor",
-      image: "https://svnmcdonald.com/wp-content/uploads/2024/09/image6-1.png",
+      image: "/team/matthew-garff.webp",
       phone: "352.274.3800",
       email: "matthew.garff@svn.com",
       bio: "Matthew Garff is an Associate Advisor at SVN | McDonald & Company in Ocala, FL. Growing up in Tampa, Florida, Matthew is from a heritage of farming and ranching, going back five generations. Today, he enjoys helping advise clients in the ever-changing commercial real estate market of North Central Florida. Matthew holds a Bachelor of Science in Economics from Brigham Young University. In his free time, he enjoys being on the water, especially on one of the many crystal clean springs that make central Florida such a great place to live and work."
@@ -27,7 +27,7 @@ const TeamPage = () => {
     {
       name: "Stiles McDonald",
       role: "5th Generation Floridian",
-      image: "https://svnmcdonald.com/wp-content/uploads/2024/09/stiles_0002_IMG_7996-1.png",
+      image: "/team/stiles-mcdonald.webp",
       phone: "352.274.3800",
       email: "stiles.mcdonald@svn.com",
       bio: "Stiles McDonald is a 5th generation Floridian born into a family of avid outdoorsmen. This resulted in instilling him with a love of land and all things real estate, which has led to him working with SVN. He is passionate about bringing value to clients, and enjoys the process from start to finish."
